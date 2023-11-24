6d4e8514da07d40f99296ba3fbe5689fedbf6692
kdong <khoidong.ca@gmail.com>
2023-11-23 23:16:53 -0600

    push to Github

---
+++ nav.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css">
+    <title>Final Project</title>
+</head>
+<body>
+  <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+      <a class="navbar-brand" href="index.php">Home</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+          <a class="nav-link" href="make.php">Make</a></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Model</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Year</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Price</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Odometer</a>
+          </li>
+        </ul>
+      </div>
+  </nav>
